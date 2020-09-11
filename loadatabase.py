b0f512cc4b1f8e2a97dedb51335c24707bf953d8
christiandavidfs <christiandavid.fs@gmail.com>
2020-09-11 00:10:50 -0300

    Add files via upload
    Fixed the upload of the jsons to the database. still needed to add testrun number and separete json data into multiple columns

--- loadatabase.py
+++ loadatabase.py
@@ -1,9 +1,11 @@
-import psycopg2
-import json
-import codecs
 import glob
+import psycopg2
+import pandas as pd
+from sqlalchemy import create_engine
+import json
+from pandas import json_normalize
 
-
+#merge jsons
 
 read_files = glob.glob("result/*.json")
 output_list = []
@@ -12,34 +14,44 @@ for f in read_files:
     with open(f, "rb") as infile:
         output_list.append(json.load(infile))
 
-with open("merged_file.json", "w") as outfile:
+with open("source.json", "w") as outfile:
     json.dump(output_list, outfile)
 
 
 # path_to_file = input('Enter path to json file:')
-with open('merged_file.json', 'r') as data_file:
+with open('source.json', 'r') as data_file:
     data = json.load(data_file)
 
 collection_array = []
 for item in data:
     collection_array.append(json.dumps(item))
 
+df = pd.read_json (r'source.json')
+export_csv = df.to_csv (r'end.csv',columns=['name','status','steps','start','stop','uuid','historyId','labels'], index = None, header=True, sep=';')
 
+# connect to the database
+conn = psycopg2.connect(host='localhost',
+                       dbname='behavee',
+                       user='postgres',
+                       password='m1jnkj03MK',
+                       port='5432')  
+#create a cursor object 
+#cursor object is used to interact with the database
+cur = conn.cursor()
 
-try:
-    conn = psycopg2.connect(database="behavee", user="postgres", password="password",host="127.0.0.1",port="5432")
-    print ("opened  database successfully")
-    cur = conn.cursor()
+#create table with same headers as csv file
+cur.execute("CREATE TABLE IF NOT EXISTS results(name text,status text,steps text,start text,stop text,uuid text,historyId text,labels text)")
 
-    for element in collection_array:
-        cur.execute("INSERT INTO results (json_column_name) VALUES (%s)", (element,))
-    print("successfully inserted records")    
-
-
-except psycopg2.Error as e:
-    raise
-
-finally:
+#open the csv file using python standard file I/O
+#copy file into the table just created 
+with open('end.csv', 'r') as f:
+    next(f) # Skip the header row.
+    #f , <database name>, Comma-Seperated
+    cur.copy_from(f, 'results', sep=';')
+    #Commit Changes
     conn.commit()
+    #Close connection
     conn.close()
-    print("connection is closed")
+
+
+f.close()
